a7addaa4b0fcfcb7a1123c35977f10463a530edf
AudunKodehode <audunkodehode@gmail.com>
2023-10-18 11:05:01 +0200

    *

--- SearchSelectedText.js
+++ SearchSelectedText.js
@@ -1,26 +1,28 @@
 const searchEngine = "https://www.google.com/search?q=";
-const toggle = document.querySelector(".toggle");
 // import {keyBind, alternativeKey} from "./settings.js";
-keyBind = "Q"
+let keyBind = "Q";
 const alternativeKey = keyBind.toLowerCase();
 let toggleValue = true;
 document.addEventListener("keydown", function (event) {
-    if ((event.key === keyBind || event.key === alternativeKey) && event.ctrlKey) {
-        let search = window.getSelection().toString();
-        if (search != "") {
-            search.replace(" ", "+")
-            window.open(searchEngine+search, '_blank');
-        }
+    if (toggleValue == true){
+  if (
+    (event.key === keyBind || event.key === alternativeKey) &&
+    event.ctrlKey
+  ) {
+    let search = window.getSelection().toString();
+    if (search != "") {
+      search.replace(" ", "+");
+      window.open(searchEngine + search, "_blank");
     }
-})
-function toggleFunction(){
-toggle=!toggle;
-if (toggle){
-    document.querySelector(".toggle").classList.add("on");
-    document.querySelector(".toggle").classList.remove("off");
+  }
 }
-else{
-    document.querySelector(".toggle").classList.add("off");
-    document.querySelector(".toggle").classList.remove("on");
-}
+});
+
+function toggleFunction() {
+  toggle = !toggle;
+  if (toggle) {
+    document.getElementById("toggleDiv").className = "toggle on";
+  } else {
+    document.getElementById("toggleDiv").className = "toggle off";
+  }
 }
