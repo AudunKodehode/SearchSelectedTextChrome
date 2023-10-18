5ddc4c89ffb20c026916a46b4ede0ebc4ad29e15
AudunKodehode <audunkodehode@gmail.com>
2023-10-18 10:43:54 +0200

    *

--- SearchSelectedText.js
+++ SearchSelectedText.js
@@ -1,8 +1,9 @@
 const searchEngine = "https://www.google.com/search?q=";
+const toggle = document.querySelector(".toggle");
 // import {keyBind, alternativeKey} from "./settings.js";
 keyBind = "Q"
 const alternativeKey = keyBind.toLowerCase();
-
+let toggleValue = true;
 document.addEventListener("keydown", function (event) {
     if ((event.key === keyBind || event.key === alternativeKey) && event.ctrlKey) {
         let search = window.getSelection().toString();
@@ -12,3 +13,14 @@ document.addEventListener("keydown", function (event) {
         }
     }
 })
+function toggleFunction(){
+toggle=!toggle;
+if (toggle){
+    document.querySelector(".toggle").classList.add("on");
+    document.querySelector(".toggle").classList.remove("off");
+}
+else{
+    document.querySelector(".toggle").classList.add("off");
+    document.querySelector(".toggle").classList.remove("on");
+}
+}
