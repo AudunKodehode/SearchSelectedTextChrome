4f8c3ef535784b8e693cfd5b3a0e07b5a4b12395
AudunKodehode <audunkodehode@gmail.com>
2023-10-18 13:04:18 +0200

    *

--- SearchSelectedText.js
+++ SearchSelectedText.js
@@ -3,74 +3,93 @@ let keyBind = "Q";
 let alternativeKey = keyBind.toLowerCase();
 const toggleButton = document.getElementById("toggleDiv");
 const saveSettingsButton = document.getElementById("saveSettingsButton");
+const modifierInput = document.getElementById("modifierInput");
 const keyInput = document.getElementById("keyInput");
 let toggleValue = true;
 
 function setToggle() {
   if (toggleValue) {
-    document.getElementById("toggleDiv").className = "toggle on";
+    toggleButton.className = "toggle on";
   } else {
-    document.getElementById("toggleDiv").className = "toggle off";
+    toggleButton.className = "toggle off";
   }
 }
 
 function toggleFunction() {
   toggleValue = !toggleValue;
-  if (toggleValue) {
-    document.getElementById("toggleDiv").className = "toggle on";
-    document.cookie[0] = "1"
-  } else {
-    document.getElementById("toggleDiv").className = "toggle off";
-    document.cookie[0] = "0"
-  }
-}
-document.addEventListener("DOMContentLoaded", function () {
   setToggle();
-  toggleButton.addEventListener("click", toggleFunction);
-  saveSettingsButton.addEventListener("click", saveSettings);
-  if (document.cookie.length > 2){
-    if (document.cookie[0] == "0"){
-        toggleValue = false;
-    } else {
-        toggleValue = true;
-    }
-    keyBind = document.cookie[1];
-    keyInput.value = document.cookie[1];
-    alternativeKey = document.cookie[2];
-    console.log(document.cookie)
-    } else{
-        setEventListener("q", "Q");
-        console.log("set standard Q")
-    }
-    console.log(`Selected keybind: ${keyBind}`);
+  // Save the toggle state here
+  document.cookie = "toggleState=" + (toggleValue ? "1" : "0");
+  console.log(`togglevalue: ${toggleValue}`);
+}
 
-});
 function saveSettings() {
   let textvalue = keyInput.value[0];
   textvalue = textvalue.toUpperCase();
   keyInput.value = textvalue;
   keyBind = textvalue;
   alternativeKey = keyBind.toLowerCase();
-  document.cookie[1] = keyBind;
-  document.cookie[2] = alternativeKey;
+  // Save key binding and alternative key
+  document.cookie = "keyBind=" + keyBind;
+  document.cookie = "alternativeKey=" + alternativeKey;
+  
+  // Save the toggle state as well
+  document.cookie = "toggleState=" + (toggleValue ? "1" : "0");
 
-setEventListener(keyBind, alternativeKey);
-
+  setEventListener(keyBind, alternativeKey);
 }
 
-function setEventListener(keyBind, alternativeKeyBind){
-    document.addEventListener("keydown", function (event) {
-        if (toggleValue == true) {
-          if (
-            (event.key === keyBind || event.key === alternativeKeyBind) &&
-            event.ctrlKey
-          ) {
-            let search = window.getSelection().toString();
-            if (search != "") {
-              search.replace(" ", "+");
-              window.open(searchEngine + search, "_blank");
-            }
-          }
-        }
-      });
+function setEventListener(keyBind, alternativeKeyBind) {
+  document.removeEventListener("keydown", keydownHandler); // Remove the previous listener
+  console.log(
+    `adding event listener with keys: ${modifierInput.value} + ${keyBind} / ${alternativeKeyBind}`
+  );
+
+  document.addEventListener("keydown", keydownHandler);
 }
+
+function keydownHandler(event) {
+  if (toggleValue) {
+    if ((event.key === keyBind || event.key === alternativeKey) && modifierCheck()) {
+      let search = window.getSelection().toString();
+      if (search !== "") {
+        search = search.replace(" ", "+"); // Fix: Replace spaces with '+'
+        window.open(searchEngine + search, "_blank");
+      }
+    }
+  }
+}
+
+function modifierCheck() {
+  if (modifierInput.value === "ctrl") {
+    return event.ctrlKey;
+  } else if (modifierInput.value === "shift") {
+    return event.shiftKey;
+  } else if (modifierInput.value === "alt") {
+    return event.altKey;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setToggle();
+  toggleButton.addEventListener("click", toggleFunction);
+  saveSettingsButton.addEventListener("click", saveSettings);
+
+  // Load settings from cookies
+  if (document.cookie.length > 0) {
+    const cookieData = document.cookie.split(";").map(cookie => cookie.trim());
+    cookieData.forEach(cookie => {
+      if (cookie.startsWith("keyBind=")) {
+        keyBind = cookie.split("=")[1];
+        keyInput.value = keyBind;
+        alternativeKey = keyBind.toLowerCase();
+      } else if (cookie.startsWith("toggleState=")) {
+        toggleValue = cookie.split("=")[1] === "1";
+        setToggle();
+      }
+    });
+  }
+
+  // Set the initial event listener
+  setEventListener(keyBind, alternativeKey);
+});
